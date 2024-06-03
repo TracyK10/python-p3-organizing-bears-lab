36eeb1cd0b4078378b40a5a09ad89379edfa2463
Tracy Karanja <chirutracy@gmail.com>
2024-06-03 17:24:13 +0300

    "Added create table and insert data for bears, and implemented two SQL queries in sql_queries.py"

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 5, "M", "brown", "angry", 1),
+(2, "Rowdy", 10, "M", "white", "short-tempered", 0),
+(3, "Tabitha", 15, "F", "white", "choleric", 1),
+(4, "Sergeant Brown", 18, "M", "cinnamon", "melancholic", 1),
+(5, "Melissa", 20, "F", "blonde", "supine", 1),
+(6, "Grinch", 30, "M", "red", "sanguine", 0),
+(7, "Wendy", 22, "F", "cream", "melancholic", 0),
+(8, NULL, 16, "F", "black", "happy", 0);
